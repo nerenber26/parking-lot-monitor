231ca6df998ccd8ec17db6784fb604f5692b7a45
Bryce Kwon <signer@brycekwon.com>
2024-11-11 15:46:39 -0800

    build: add docker-compose file to deploy server

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Parking Lot Monitor
+
+## Installation
+
+```bash
+docker network create parking-system-network
+
+docker compose up -d
+```
+
+## Citations
+
+- [Chirpstack Docker Compose](https://github.com/chirpstack/chirpstack-docker/)
---
+++ server/utils/connectDB.js
@@ -0,0 +1,58 @@
+import pg from 'pg';
+
+
+const { Pool } = pg;
+
+const POSTGRES_HOST = process.env.POSTGRES_HOST || 'localhost';
+const POSTGRES_PORT = process.env.POSTGRES_PORT || 5432;
+const POSTGRES_MAX_CONNECTIONS = process.env.POSTGRES_MAX_CONNECTIONS || 20;
+const POSTGRES_IDLE_TIMEOUT = process.env.POSTGRES_IDLE_TIMEOUT || 30000;
+const POSTGRES_CONNECTION_TIMEOUT = process.env.POSTGRES_CONNECTION_TIMEOUT || 2000;
+
+const POSTGRES_USER = process.env.POSTGRES_USER || console.error('POSTGRES_USER is not set') && process.exit(1);
+const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD || console.error('POSTGRES_PASSWORD is not set') && process.exit(1);
+const POSTGRES_DATABASE = process.env.POSTGRES_DATABASE || console.error('POSTGRES_DATABASE is not set') && process.exit(1);
+
+const pool = new Pool({
+    host: POSTGRES_HOST,
+    port: POSTGRES_PORT,
+    user: POSTGRES_USER,
+    password: POSTGRES_PASSWORD,
+    database: POSTGRES_DATABASE,
+    max: POSTGRES_MAX_CONNECTIONS,
+    idleTimeoutMillis: POSTGRES_IDLE_TIMEOUT,
+    connectionTimeoutMillis: POSTGRES_CONNECTION_TIMEOUT,
+});
+
+/**
+ * Executes a SQL query against a database.
+ *
+ * @async
+ * @function runQuery
+ * @param {string} text SQL query to be executed
+ * @param {Array} params Array of parameters to use in the SQL query
+ * @returns {Promise<Object>} Promise that resolves to the result of the query
+ *
+ * @example
+ * const result = await runQuery('SELECT * FROM users WHERE id = $1', [userId]);
+ * console.log(result.rows);
+ */
+export const runQuery = async(text, params) => {
+    return pool.query(text, params);
+};
+
+/**
+ * Retrieves a database client from the connection pool, overriding its release methods.
+ *
+ * @async
+ * @function getClient
+ * @returns {Promise<Object>} Promise that resolves to a database client
+ *
+ * @example
+ * const client = await getClient()
+ * const result = await client.query('SELECT * FROM users');
+ * client.release()
+ */
+export const getClient = async () => {
+    return pool.connect();
+};
