614f3a71009b4b8fb78c32dc3504a17e9e89992d
nerenber26 <112669015+nerenber26@users.noreply.github.com>
2024-11-12 16:47:48 -0800

    home view reads from infoArray

--- README.md
+++ README.md
@@ -11,3 +11,8 @@ docker compose up -d
 ## Citations
 
 - [Chirpstack Docker Compose](https://github.com/chirpstack/chirpstack-docker/)
+
+
+
+
+Lots: Main, Commons, Waldschmidt, Shiley, Christie, Kenna, Portsmouth
--- server/index.js
+++ server/index.js
@@ -12,7 +12,17 @@ app.set('view engine', 'pug');
 app.use(express.static(join(__dirname, 'public')));
 
 app.get('/', async(req, res) => {
-    res.status(200).render('home');
+    const infoArray = [
+        'Main Lot: 50/100 Available &#x1F7E9', 
+        'Commons Lot: 5/40 Available &#x1F7E5', 
+        'Waldschmidt Lot: 10/30 Available &#x1F7E8',
+        'Shiley Lot: 10/20 Available &#x1F7E9',
+        'Christie Lot: 5/20 Available &#x1F7E5',
+        'Kenna Lot: 1/20 Available &#x1F7E5',
+        'Portsmouth Lot: 2/20 Available &#x1F7E5',
+        ]
+    console.log(infoArray)
+    res.status(200).render('home', { infoArray });
 });
 
 app.get('/map', async(req, res) => {
