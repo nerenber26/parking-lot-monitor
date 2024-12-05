7f3b43da1fc5f99acf07b901bfba1ce93ebf645f
Bryce Kwon <signer@brycekwon.com>
2024-12-04 16:22:53 -0800

    fix: update values to display all colors

--- website/server/routes/pages.py
+++ website/server/routes/pages.py
@@ -11,11 +11,11 @@ def home_page():
     rows = [
         ("Main Lot", 100, 77),
         ("Bauccio Commons Lot", 50, 10),
-        ("Waldschmidt Lot", 75, 0),
+        ("Waldschmidt Lot", 75, 33),
         ("Shiley Lot", 120, 120),
         ("Christie Lot", 60, 0),
         ("Kenna Lot", 30, 0),
-        ("Portsmouth Lot", 40, 0),
+        ("Portsmouth Lot", 40, 20),
     ]
     return render_template("home.html", rows=rows)
     # result = db.session.execute(text("""
