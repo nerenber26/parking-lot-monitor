360a344cbb140505f52b4bd9cad9ac96dc4cd175
Bryce Kwon <me@brycekwon.com>
2025-01-18 01:46:17 -0800

    feat: add sentry.io analytics

--- website/server/wsgi.py
+++ website/server/wsgi.py
@@ -1,10 +1,22 @@
+from os import environ
+
 from flask import Flask, render_template
 
 from app.pages.base import pages_bp
 from app.api.base import api_bp
 
+import sentry_sdk
+
 
 def create_app():
+    sentry_sdk.init(
+        dsn=environ.get('SENTRY_DSN'),
+        traces_sample_rate=1.0,
+        _experiments={
+            "continuous_profiling_auto_start": True,
+        },
+    )
+
     app = Flask(__name__, static_url_path="")
 
     app.register_blueprint(pages_bp, url_prefix="/")
